28b5054e59bfcd60d9075559c1770ac7f538274c
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-29 03:47:42 +0000

    Make DerbyLeague.profile_url private

--- spec/derby_league_spec.rb
+++ spec/derby_league_spec.rb
@@ -7,17 +7,19 @@ RSpec.describe DerbyLeague do
                              is_full_member: true,
                              profile_url: "https://wftda.com/wftda-leagues" \
                                "/atlanta-rollergirls/" })
+  league.load_details
 
   it "should have attr_readers for name, country, city, is_full_member, " \
-    "profile_url, website, and game_recaps" do
+    "website, and game_recaps" do
 
     expect(league).to respond_to(:name, :country, :city, :is_full_member, \
-      :profile_url, :website, :game_recaps)
+      :website, :game_recaps)
     expect(league).not_to respond_to(:name=, :country=, :city=, \
-      :is_full_member=, :profile_url=, :website=, :game_recaps=)
+      :is_full_member=, :website=, :game_recaps=)
   end
 
   it "should load website and game_recaps in from the profile_url" do
+
     expect(league.website).to eq("http://www.atlantarollergirls.com/")
     expect(league.game_recaps).to all( be_an_instance_of(GameRecap) )
   end
