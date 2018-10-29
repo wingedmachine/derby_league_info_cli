3c9d445f348d14fba16bfe58ca8b901822e831d0
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-29 02:34:30 +0000

    Create CountryList and tests

--- lib/country_list.rb
+++ lib/country_list.rb
@@ -1,3 +1,22 @@
-class CountryList
+require 'scraper/country_codes'
 
+class CountryList
+  attr_reader :countries
+
+  def initialize(countries)
+    @countries = countries
+  end
+
+  def find_by_code(code)
+    countries[code.to_sym]
+  end
+
+  def search_by_name(input)
+    CountryList.new(countries.select { |key, value| value.upcase.include? \
+      (input.upcase) })
+  end
+
+  def ==(other)
+    self.class == other.class && self.countries == other.countries
+  end
 end
--- spec/country_list_spec.rb
+++ spec/country_list_spec.rb
@@ -1,7 +1,29 @@
 require "spec_helper"
 
 RSpec.describe CountryList do
-  it "returns a country's full name when supplied its code" do
-    expect(false).to be_true
+  country_list = CountryList.new(Scraper::CountryCodes.scrape)
+
+  it "#find_by_code returns a country's full name when supplied its code" do
+    expect(country_list.find_by_code("AF")).to eq("Afghanistan")
+    expect(country_list.find_by_code("SV")).to eq("El Salvador")
+    expect(country_list.find_by_code("FI")).to eq("Finland")
+    expect(country_list.find_by_code("IN")).to eq("India")
+    expect(country_list.find_by_code("JP")).to eq("Japan")
+    expect(country_list.find_by_code("TR")).to eq("Turkey")
+    expect(country_list.find_by_code("ZW")).to eq("Zimbabwe")
+  end
+
+  it "#search_by_name returns a new CountryList of countries whose name " \
+    "matches the supplied string, ignoring case" do
+
+    results = CountryList.new({ TZ: "Tanzania, United Republic of",
+                                AE: "United Arab Emirates",
+                                GB: "United Kingdom",
+                                US: "United States of America" })
+    sub_results = CountryList.new({ TZ: "Tanzania, United Republic of",
+                                    US: "United States of America" })
+
+    expect(country_list.search_by_name("united")).to eq(results)
+    expect(results.search_by_name("OF")).to eq(sub_results)
   end
 end
