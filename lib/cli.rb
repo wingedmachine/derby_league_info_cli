38a4cc65044103649a9929b327deeb098f026717
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-29 21:54:31 +0000

    Adds crude version of main Cli loop
    "Crude" because it's primitive, but also becasue it says "fuck"

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,2 +1,32 @@
 class Cli
+  def start
+    greet
+    load_initial
+    load_complete
+    main_loop
+    goodbye
+  end
+
+  def greet
+    puts "Welcome to thing, wait for load..."
+  end
+
+  def load_initial
+  end
+
+  def load_complete
+    puts "Load done thins will start"
+  end
+
+  def main_loop
+    input = nil
+    until input == "exit"
+      puts "Waht do?"
+      input = gets.downcase.strip
+    end
+  end
+
+  def goodbye
+    puts "fuck outta here"
+  end
 end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,16 +1,23 @@
 require "spec_helper"
 
 RSpec.describe Cli do
-  xit "greets the user" do end
-
-  xit "gets the league data" do end
-
-  xit "creates a league list" do end
+  it "greets the user" do end
 
   xit "gets the country code data" do end
 
   xit "creates a country list" do end
 
+  xit "gets the league data" do end
+
+  xit "creates a league list" do end
+
   xit "runs until the user inputs 'exit'" do end
 
-  end
+  xit "displays paginated list of leagues" do end
+
+  xit "displays paginated list of countries" do end
+
+  xit "will display paginated list of leagues in a country selected from the list" do end
+
+  xit "will display details about a league selected from the list" do end
+end
