5f4e9018a4348e6c1a33e1a96ca251af1afb82b5
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-29 00:29:35 +0000

    Add comment explaining task left undone

--- lib/game_recap.rb
+++ lib/game_recap.rb
@@ -2,6 +2,11 @@ class GameRecap
   attr_reader :author, :datetime, :year, :event, :location, :game_number, \
     :teams, :url
 
+  # Ideally, the strings in @teams would be replaced by the League objects
+  # they refer to, but correlating a string containing a league's nickname
+  # to an object containing it's full name is a non-trivial task beyond the
+  # scope of this assignment.
+
   def initialize(recap)
     @author = recap[:author]
     @datetime = recap[:datetime]
