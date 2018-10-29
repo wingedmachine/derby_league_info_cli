304feff4e87e7a54d8b109843daedbb528b9b9af
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-29 00:24:32 +0000

    Create GameRecap class

--- lib/game_recap.rb
+++ lib/game_recap.rb
@@ -1,6 +1,15 @@
 class GameRecap
-  attr_reader
+  attr_reader :author, :datetime, :year, :event, :location, :game_number, \
+    :teams, :url
 
   def initialize(recap)
+    @author = recap[:author]
+    @datetime = recap[:datetime]
+    @year = recap[:year]
+    @event = recap[:event]
+    @location = recap[:location]
+    @game_number = recap[:game_number]
+    @teams = recap[:teams]
+    @url = recap[:url]
   end
 end
