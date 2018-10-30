d5e42480458855dc6ab21da0e015549a06f3c0e6
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-30 20:51:34 +0000

    Rewrite Comparable tests to be independent
    Previous version relied on specifics of the LeagueList implement-
    ation, and so broke when I made changes there. This version has
    no such dependencies.

--- spec/comparable_spec.rb
+++ spec/comparable_spec.rb
@@ -4,15 +4,17 @@ RSpec.describe Comparable do
   context "InstanceMethods" do
     let(:raw_atl_subset) { Scraper::LeagueList.scrape( \
       "https://wftda.com/?s=atlanta&post_type=leagues") }
-    let(:atl_subset_1) { LeagueList.create_from_hash_array(raw_atl_subset) }
-    let(:atl_subset_2) { LeagueList.create_from_hash_array(raw_atl_subset.dup) }
-    let(:atl_sub_subset) { LeagueList.create_from_hash_array( \
-      raw_atl_subset[0..-2]) }
+    let(:object_1) { object = Class.new { include Comparable::InstanceMethods }
+                     object.instance_variable_set("@str", "some text")}
+    let(:object_2) { object = Class.new { include Comparable::InstanceMethods }
+                     object.instance_variable_set("@str", "some text")}
+    let(:other_object) { object = Class.new { include Comparable::InstanceMethods }
+                    object.instance_variable_set("@str", "some different text")}
 
     it "correctly determines whether two objects are equal without regard " \
       "for the memory addresses of it or its parts" do
-      expect(atl_subset_1).to eq(atl_subset_2)
-      expect(atl_subset_1).not_to eq(atl_sub_subset)
+      expect(object_1).to eq(object_2)
+      expect(object_1).not_to eq(other_object)
     end
   end
 end
